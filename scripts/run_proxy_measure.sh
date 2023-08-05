de454edc5639387b6284b5a14966a0c90e7b6de3
Marvin Ewald <marvin.e@protonmail.com>
2023-08-06 00:19:27 +0200

    Plot bars for proxy computation delay

--- scripts/run_proxy_measure.sh
+++ scripts/run_proxy_measure.sh
@@ -10,9 +10,24 @@ fi
 
 dir="$PWD"
 
+sudo -v
+
+trap cleanup EXIT INT HUP TERM
+
+cleanup() {
+    # shellcheck disable=2046
+    sudo kill -INT $(jobs -p)
+    sleep 1
+    # shellcheck disable=2046
+    sudo kill -9 $(jobs -p)
+    killall ffplay
+    killall ffmpeg
+    killall udp-proxy
+}
+
 cd ../udp-proxy/ || exit 1
-go run . -l 5004 -r 6004 -d 1000 --csv "$dir/proxy_$NAME.csv" "$@" &
-# go run . -l 5004 -r 6004 --csv "measure_$NAME.csv" &
+# go run . -l 5004 -r 6004 -d 1000 --csv "$dir/proxy_$NAME.csv" "$@" &
+go run . -l 5004 -r 6004 --csv "$dir/proxy_$NAME.csv" "$@" &
 cd - || exit 1
 
 # ./ffmpeg_play.sh example_5004.sdp > /dev/null 2>&1 &
@@ -20,6 +35,9 @@ cd - || exit 1
 
 # ./udp.py -l localhost 5004 &
 # ./udp.py -l localhost 6004 &
+
+sudo tcpdump -i lo -U -w "$dir/${NAME}_to_proxy.pcap" udp and dst port 5004 &
+sudo tcpdump -i lo -U -w "$dir/${NAME}_from_proxy.pcap" udp and dst port 6004 &
 
 ./log_cpu.sh "$dir/cpu_$NAME.csv" &
 
@@ -31,10 +49,4 @@ cd - || exit 1
 ffmpeg -re -i example_short.mp4 -an -f rtp rtp://127.0.0.1:5004
 sleep 1
 
-# shellcheck disable=2046
-kill -INT $(jobs -p)
-sleep 1
-kill -9 $(jobs -p)
-killall ffplay
-killall ffmpeg
-killall udp-proxy
+cleanup
